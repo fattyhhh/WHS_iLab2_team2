95c42a2773599a52c9c43a0826125bd5c0883b45
Tingwei <100115750+fattyhhh@users.noreply.github.com>
2023-09-27 14:02:31 +1000

    updated read_urls script

--- read_urls.py
+++ read_urls.py
@@ -1,10 +1,9 @@
-import requests
-import json
-import re
-import pandas as pd
+mport pandas as pd
 from bs4 import BeautifulSoup
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+from urllib.parse import urljoin
+import xlsxwriter
 
 # set headers
 headers = {
@@ -23,29 +22,50 @@ headers = {
 all_data = []
 
 # import website and company name from excel file
-urls = pd.read_excel('yellowpages2.xlsx')
+urls = pd.read_excel('yellowpages5.xlsx')
+# select the rows to be scraped
+urls = urls.iloc[953:958,:]
 urls = urls[['name','website']]
-
+urls['text'] = ''
+urls['sublinks'] = ''
+urls['sublinkstext'] = ''
 # scrape website text and store in dataframe
-# just demo for now
+# something wrong with index 958
 with requests.Session() as s:
-    for i in range(2,4):
+    # manual change the range
+    for i in range(953, 958):
         try:
+            print(i)
+            temp = urls['website'][i].replace('https://www.','')
             r = s.get(urls['website'][i], verify=False, headers=headers)
             soup = BeautifulSoup(r.text, 'html.parser')
             text = soup.get_text()
             text = text.replace('\n', '').replace('\t', '')
-            links = soup.find_all('a', href=True)
-            links = [link['href'] for link in links]
-            links = [link for link in links if (link.startswith(urls['website'][i]) and link.endswith('/'))]
-            #links2 = [link for link in links if link.startswith('/')]
-            #links3 = []
-            #for x in len(links2):
-            #    links3[x] = urls['website'][i] + links2[x]
-            #print(links3)
-            #links = links1 + links3
+            sublinks=[]
+            for link in soup.find_all('a', href=True):
+                absolute_link = urljoin(urls['website'][i], link['href'])
+                if absolute_link not in sublinks:
+                    if temp in absolute_link:
+                        sublinks.append(absolute_link)
+                
             urls['text'][i] = text
-            urls['son_links'][i] = links
-        except Exception as e:
-            urls['text'] = 'error'
-            pass    
+            urls['sublinks'][i] = sublinks
+            for x in urls['sublinks'][i]:
+                try:
+                    t = s.get(x, verify=False, headers=headers)
+                    soup1 = BeautifulSoup(t.text, 'html.parser')
+                    text1 = soup1.get_text()
+                    text1 = text1.replace('\n', '').replace('\t', '')
+                    urls['sublinkstext'][i] = urls['sublinkstext'][i] + text1
+                except:
+                    print('error in sublinks')
+                    print(i)
+                    pass 
+        except:
+            print('error')
+            print(i)
+            pass
+
+# need manually amend the suffix 
+urls.to_excel('all_text_6_5.xlsx', engine='xlsxwriter')
+           
